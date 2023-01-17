cfa4f95fafb3a9ec3479cca62601a8174c191552
Saulo Joel Oliveira Leite <sjoldeveloper@gmail.com>
2023-01-17 20:39:33 -0300

    CREATE

---
+++ CRUD-JAVA/src/ConexaoDAO.java
@@ -0,0 +1,26 @@
+
+import java.sql.Connection;  // Importando biblioteca necessária para criar a conexão Java - SQL
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+ /*
+ * @author saulo.leite
+ */
+
+public class ConexaoDAO { // 
+    public Connection conectaBD(){ //Retorna uma variável de conexão
+        Connection conn = null; // Var utilizada na conexão 
+        
+        //Tentativa e excessão 
+        try {
+            String url = "jdbc:mysql://localhost:3306/bancoteste?user=root&passwprd="; //URL utilizada para conexão
+            conn = DriverManager.getConnection(url); // Armazeando na variável todaa informação da URL
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "ConexaoDAO" + erro.getLocalizedMessage()); //Messagem de erro
+        }
+        
+        return conn;
+    
+    }
+}
